d19eda25a572c45d5ee67b7802ded8fbe9ffca55
Danny Espinoza <danny.espinozasanchez@fiserv.com>
2022-09-27 21:40:50 -0600

    first commit Login screen

---
+++ GameBoardSearcher/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  GameBoardSearcher
+//
+//  Created by Danny Espinoza Sanchez on 9/22/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
